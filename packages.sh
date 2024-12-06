32967a9f7a01b0ac1d388ce1a6a70f89c892298f
Jay Joshua <joejoshw@gmail.com>
2024-12-06 16:47:23 +0100

    Include .oh-my-zsh

--- packages.sh
+++ packages.sh
@@ -1,10 +1,10 @@
-pacman -S zdg-user-dirs polybar i3 rofi lsb-release hyprland sway power-profiles-daemon hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
-pacman -S kitty zsh neovim
-pacman -S unzip rofi-wayland thunar
+sudo pacman -S zdg-user-dirs polybar i3 rofi lsb-release hyprland sway power-profiles-daemon hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
+sudo pacman -S kitty zsh neovim
+sudo pacman -S unzip rofi-wayland thunar
 
 yay -S google-chrome waybar grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat hyprsunset-git hypridle-git
 
-sudo systemctl enable power-profiles-daemon
-sudo systemctl start power-profiles-daemon
+#sudo systemctl enable power-profiles-daemon
+#sudo systemctl start power-profiles-daemon
 
-curl -fsSL https://bun.sh/install | bash
+#curl -fsSL https://bun.sh/install | bash
