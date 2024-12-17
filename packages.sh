cbfaadc50976de88b96636ebc5823a098db85e10
Jay Joshua <joejoshw@gmail.com>
2024-12-17 16:35:26 +0100

    Add some new packages

--- packages.sh
+++ packages.sh
@@ -1,10 +1,13 @@
-sudo pacman -S zdg-user-dirs polybar i3 rofi lsb-release hyprland sway power-profiles-daemon hyprpaper pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
-sudo pacman -S kitty zsh neovim
+sudo pacman -S zdg-user-dirs polybar i3 rofi lsb-release hyprland
+sudo pacman -S sway power-profiles-daemon hyprpaper pipewire libgtop
+sudo pacman -S bluez bluez-utils btop networkmanager dart-sass wl-clipboard
+sudo pacman -S brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
+sudo pacman -S kitty zsh neovim btop
 sudo pacman -S unzip rofi-wayland thunar
 
 yay -S google-chrome waybar grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat hyprsunset-git hypridle-git
-
-yay -S nwg-dock-hyprland nwg-drawer
+yay -S nerd-fonts slack-desktop aws-cli-v2 visual-studio-code-bin
+yay -S nwg-dock-hyprland nwg-drawer expressvpn
 
 #sudo systemctl enable power-profiles-daemon
 #sudo systemctl start power-profiles-daemon
