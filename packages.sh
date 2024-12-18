887ca27a79edef2052b67901b07d8ac56e9bd172
Jay Joshua <joejoshw@gmail.com>
2024-12-18 07:12:57 +0100

    add yay to init script

--- packages.sh
+++ packages.sh
@@ -1,15 +1,22 @@
-sudo pacman -S zdg-user-dirs polybar i3 rofi lsb-release hyprland
-sudo pacman -S sway power-profiles-daemon hyprpaper pipewire libgtop
+sudo pacman -S --needed git base-devel
+
+mkddir -p code
+cd code
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+sudo pacman -S --needed zdg-user-dirs polybar i3 rofi lsb-release hyprland sway power-profiles-daemon hyprpaper pipewire libgtop
 sudo pacman -S bluez bluez-utils btop networkmanager dart-sass wl-clipboard
 sudo pacman -S brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon gvfs
-sudo pacman -S kitty zsh neovim btop
-sudo pacman -S unzip rofi-wayland thunar
+sudo pacman -S kitty zsh neovim btop unzip rofi-wayland thunar
 
 yay -S google-chrome waybar grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat hyprsunset-git hypridle-git
 yay -S nerd-fonts slack-desktop aws-cli-v2 visual-studio-code-bin
 yay -S nwg-dock-hyprland nwg-drawer expressvpn
 
-#sudo systemctl enable power-profiles-daemon
-#sudo systemctl start power-profiles-daemon
+sudo systemctl enable power-profiles-daemon
+sudo systemctl start power-profiles-daemon
 
 #curl -fsSL https://bun.sh/install | bash
